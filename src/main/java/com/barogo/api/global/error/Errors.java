4767143b503efc92636e2875d5b5e14de9421b4a
ieunune <une@kakao.com>
2023-03-21 23:19:37 +0900

    [Chore] 에러 코드 오타 수정

--- src/main/java/com/barogo/api/global/error/Errors.java
+++ src/main/java/com/barogo/api/global/error/Errors.java
@@ -4,7 +4,7 @@ import java.util.HashMap;
 
 public enum Errors {
 
-    NOT_FOUND_USER("존재하지 않는 사용자 정보", "다른 아이디와 밀번호로 다시 시도해주세요."),
+    NOT_FOUND_USER("존재하지 않는 사용자 정보", "다른 아이디와 비밀번호로 다시 시도해주세요."),
     ALREADY_USER("이미 가입되어 있는 아이디", "다른 아이디로 다시 시도해주세요."),
     INVALID_PASSWORD("잘못된 패스워드", "비밀번호는 영어 대문자, 소문자, 특수문자 중 3종류 이상으로 12자리 이상의 문자열로 생성해야합니다."),
     ;
