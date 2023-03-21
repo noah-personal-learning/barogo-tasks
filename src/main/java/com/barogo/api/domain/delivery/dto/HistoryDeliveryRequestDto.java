911da6448bec561796642b75397d880cace49764
ieunune <une@kakao.com>
2023-03-21 23:18:57 +0900

    [Setup] 배송 API 구조 세팅 DTO 추가

---
+++ src/main/java/com/barogo/api/domain/delivery/dto/HistoryDeliveryRequestDto.java
@@ -0,0 +1,12 @@
+package com.barogo.api.domain.delivery.dto;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Getter
+@ToString
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class HistoryDeliveryRequestDto {
+}
